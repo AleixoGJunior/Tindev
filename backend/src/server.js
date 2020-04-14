9a7db25e440feed3c83faa7f9c4f389cb96b8075
Aleixo G Junior <aleixo.cco@gmail.com>
2020-04-14 09:23:04 -0300

    Implementado notificação em realtime com websocket

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,15 +4,31 @@ const cors = require("cors");
 
 const routes = require("./routes");
 
-const server = express();
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+  const { user } = socket.handshake.query;
+  connectedUsers[user] = socket.id;
+});
 
 mongoose.connect(
   "mongodb://tindev:tindev@cluster0-shard-00-00-hvcvu.mongodb.net:27017,cluster0-shard-00-01-hvcvu.mongodb.net:27017,cluster0-shard-00-02-hvcvu.mongodb.net:27017/tindev?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use((request, response, next) => {
+  request.io = io;
+  request.connectedUsers = connectedUsers;
+
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
